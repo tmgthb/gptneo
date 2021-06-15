0b941a918bda541a0278d0bdf01600f9e56fe6ae
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-15 23:58:33 +0200

    Update gptneo.py

--- gptneo.py
+++ gptneo.py
@@ -7,10 +7,7 @@ st.title('GPT-Neo')
 st.text('125 Million Parameter model')
 
 @st.cache(suppress_st_warning=True,ttl=1000)
-def modelgpt():
-  prompt_text = st.text_input(label="Type the beginning words ", value="Type here")
-  generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
-  gpt_text = generator(prompt_text, do_sample=True, min_length=50)
-  return st.text(gpt_text)
-
-modelgpt()
+prompt_text = st.text_input(label="Type the beginning words ", value="Type here")
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
+gpt_text = generator(prompt_text, do_sample=True, min_length=50)
+return st.text(gpt_text)
