642c098fd547267c470206fdd069c00d5157ba5a
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-16 00:11:34 +0200

    Update gptneo.py

--- gptneo.py
+++ gptneo.py
@@ -4,7 +4,7 @@ import streamlit as st
 import torch
 from transformers import pipeline, AutoModelForCausalLM
 st.title('GPT-Neo')
-st.text('This GPT-Neo model includes 125 Million parameters and while it is not the largest available - it cna be run in Streamlit without additional resources.')
+st.text('This GPT-Neo model includes 125 Million parameters.')
 prompt_text = st.text_input(label="Add here phrase, which you want to be completed", value="Add here few words")
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M')
 gpt_text = generator(prompt_text, do_sample=True, min_length=50)
